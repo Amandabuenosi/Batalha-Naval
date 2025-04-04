8c2e3961f0e5f47309de3b3488ac1d658e734544
Amanda Bueno <202502992009@alunos.estacio.br>
2025-04-04 00:56:10 -0300

    MATRIZES INICIADAS

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Batalha-Naval
- Vetores e matrizes.
+ Batalha naval 🚢
+
+ 🎮 Esse game foi desenvolvido durante a aula da Introdução a Programação. 
+
+ Utilizando a linguagem C e desbravando os vetores e matrizes. ⬆️
+
+ 
+
+
+
+Aproveite o momento!
+
+🛠️ Construído com VSCode.
+
+Com ❤️ por Amanda Bueno
---
+++ batalhaNaval.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main (){
+
+    int tabuleiro[10][10] = { //tabuleiro de água criado 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+    char linhaAbc [][10] = {
+        {A, B, C, D, E, F, G, H, I, J}
+    }
+    int navioUm [3][1] = { //navio um na vertical ocupando 3 posições no tabuleiro.
+        {3},
+        {3},
+        {3}
+    };
+
+    int navioDois [1][3] = {//navio dois na horizontal ocupando 3 posições no tabuleiro.
+        {3, 3, 3}
+
+    };
+    printf("~~~BATALHA NAVAL~~~\n");
+    for (int linha=0; linha < 10; linha++){
+        for (int coluna = 0; coluna < 10 ; coluna++){
+            printf("%d ", tabuleiro[linha][coluna]);
+        }
+        
+        printf("\n");  // Nova linha ao fim de cada linha da matriz
+        
+    }
+    
+    return 0;
+}
