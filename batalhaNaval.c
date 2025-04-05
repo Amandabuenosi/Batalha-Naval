5a1fabcb05514164bdd36c213cdfd01b2c162425
Amanda Bueno <202502992009@alunos.estacio.br>
2025-04-05 00:08:16 -0300

    tabuleiro concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,23 +2,11 @@
 
 int main (){
 
-    int tabuleiro[10][10] = { //tabuleiro de água criado 
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    };
+    int tabuleiro[10][10] = {0};  //tabuleiro inicializado com zeros
+    
+    char colunas []= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    char linhaAbc [][10] = { //--------------------------------------- AQUI
-        {A, B, C, D, E, F, G, H, I, J}
-    }
-    int navioUm [3][1] = { //navio um na vertical ocupando 3 posições no tabuleiro.
+        int navioUm [3][1] = { //navio um na vertical ocupando 3 posições no tabuleiro.
         {3},
         {3},
         {3}
@@ -26,17 +14,37 @@ int main (){
 
     int navioDois [1][3] = {//navio dois na horizontal ocupando 3 posições no tabuleiro.
         {3, 3, 3}
-
     };
-    printf("~~~BATALHA NAVAL~~~\n");
-    for (int linha=0; linha < 10; linha++){
-        for (int coluna = 0; coluna < 10 ; coluna++){
-            printf("%d ", tabuleiro[linha][coluna]);
-        }
+
+
+    // Exibições**********************************
+
+
+    printf("~~~~~~~~~BATALHA NAVAL~~~~~~~~\n");
+    printf("  "); // espaço para alinhar o inicio do nome das colunas.
+
+    for ( int c = 0; c < 10; c++){ // linha com a nomeação das colunas.
+        printf("  ");
+        printf("%c", colunas[c]);};
         
-        printf("\n");  // Nova linha ao fim de cada linha da matriz
+
+    printf("\n"); // Inica novo parágrafo com o tabuleiro.
+
+    for (int linha = 0; linha < 10; linha++){
+        printf("%2d",linha +1);//numero das linhas somando 1 a cada loop.
         
-    }
+            for (int tab = 0; tab < 10; tab++){ //Após colocar o numero da linha, soma um em 'linha' e 'tab', assim, mudando a coordenada da matriz p/ exibir.
+                
+                printf("  %d", tabuleiro[tab][linha]);
+
+            }
+            
+            printf("\n"); // Inica novo parágrafo quando chega em 1x10, 2x10... 
+            
+    };
+    
+   
     
     return 0;
 }
+//
