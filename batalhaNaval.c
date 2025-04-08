3a6462f7af23b6c5fc432b072791652f3068cc9f
Amanda Bueno <202502992009@alunos.estacio.br>
2025-04-08 13:46:49 -0300

    cone,octaedro e cruz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -35,7 +35,7 @@ int main (){
     // Matrizes de Habilidade **********************************
     int cone[5][5], cruz[5][5], octaedro[5][5];
 
-    // Cone (5x5): origem no topo, abre para baixo
+    // Cone (5x5): origem no topo, abre para baixo.
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             if (i == 0 && j == 2) cone[i][j] = 1;
